cb7768c103da2db8b91fd10feac14c47f2b14a36
Stexe23 <98385650+Stexe23@users.noreply.github.com>
2022-02-09 18:16:35 +0500

    Распределение методов класса Board

--- pole.py
+++ pole.py
@@ -1,14 +1,27 @@
 # Создаём классы
 class Dot:  # Класс точек на поле
-    pass
+    def __init__(self, ix, iy, d):
+        self.ix = ix
+        self.iy = iy
+        self.d = d
 
 
 class Board:  # Класс поля
-    pass
+    def __init__(self, pole, ship_, hid, ship_live):
+        self.pole = pole
+        self.ship_ = ship_
+        self.hid = bool(hid)
+        self.ship_live = ship_live
+
+    def get_add_ship(self):
+        pass
+
+    def get_contour(self):
+        pass
 
 
 class Ship:  # Родительский класс кораблей
-    def ship(self, dlina, n_dot, position, live):
+    def __init__(self, dlina, n_dot, position, live):
         self.dlina = dlina
         self.n_dot = n_dot
         self.position = position
@@ -16,6 +29,11 @@ class Ship:  # Родительский класс кораблей
 
     def get_dots(self):
         return self.dlina
+
+    def get_dots(self):
+        pass
+
+
 
 
 class Linkor(Ship):  # Класс корабля на четыре клетки поля
