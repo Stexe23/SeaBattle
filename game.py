45ec8d3a94b6035362db8208b53514867133e716
Stexe23 <98385650+Stexe23@users.noreply.github.com>
2022-02-10 12:25:01 +0500

    Добавлен метод contour

--- game.py
+++ game.py
@@ -1,31 +1,7 @@
-import random
-
-field = [["0"] * 3 for i in range(7)]
-
-def vvod():
-    while True:
-        a = input('Cделайте свой ход\t').split()
-        if len(a) != 2:
-            print(" Введите 2 координаты! ")
-            continue
-
-        x, y = a
-
-        if not (x.isdigit()) or not (y.isdigit()):
-            print(" Введите числа! ")
-            continue
-
-        x, y = int(x), int(y)
-
-        if x < 1 or x > 6 or y < 1 or y > 6:
-            print('Координаты вне диапозона! Попробуёте ещё раз.')
-            continue
-
-        if field[x][y] != "0":
-            print(" Клетка занята! ")
-            continue
-
-        return x, y
 
 
-print(vvod())
+
+
+
+b = Board()
+print(b)
--- pole.py
+++ pole.py
@@ -1,37 +1,101 @@
+from random import randint
+
 # Создаём классы
+# Классы исключений
+class BoardException(Exception): # Основной клас исключения
+    pass
+
+class BoardOutException(BoardException): # Исключение выхода за поле
+    def __str__(self):
+        return "Вы пытаетесь выстрелить за доску!"
+
+class BoardUsedException(BoardException): # Исключение повторного выстрела
+    def __str__(self):
+        return "Вы уже стреляли в эту клетку"
+
+class BoardWrongShipException(BoardException): # Исключение ошибки корабля на поле
+    pass
+
+# Основные классы
 class Dot:  # Класс точек на поле
-    def __init__(self, ix, iy, d):
-        self.ix = ix
-        self.iy = iy
+    def __init__(self, x, y, d):
+        self.x = x
+        self.y = y
         self.d = d
+
+    def __eq__(self, other): # Метод для сравнения координат
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self): # Метод вывода координат поля
+        return f"Dot({self.x}, {self.y})"
 
 
 class Board:  # Класс поля
-    def __init__(self, pole, ship_, hid, ship_live):
+    def __init__(self, pole = 6, hid = False):
         self.pole = pole
-        self.ship_ = ship_
-        self.hid = bool(hid)
-        self.ship_live = ship_live
+        self.ships = [] # Хранение кораблей
+        self.busy = [] # Хранение занятых точек
+        self.hid = hid
+        self.fild = [["0"] * pole for _ in range(pole)]
 
-    def get_add_ship(self):
-        pass
+    def __str__(self): # Метод формирования поля
+        raz = ""
+        raz += "\t| 1 | 2 | 3 | 4 | 5 | 6 |"
+        for i, j in enumerate(self.fild):
+            raz += f"\n  {i + 1} | " + " | " .join(j) + " |"
+        if self.hid:
+            raz = raz.replace("■", "0")
+        return raz
 
-    def get_contour(self):
-        pass
+    def out(self, d): # Метод проверки точки за пределами доски
+        return not((0 <= d.x <= self.pole) and (0 <= d.y <= self.pole))
+
+    def add_ship(self, ship): # Метод добавления корабля на поле
+        for d in ship.dots:
+            if self.out(d) or d in self.busy: # Точка кораюля не выходит за границы и не  занята
+                raise BoardWrongShipException()
+        for d in ship.dots: # Замена точки корабля на символ
+            self.fild[d.x][d.y] = "■"
+
+    def contour(self, ship_cont, tor =False): # Метод обвода контура корабля
+        cont = [(-1, -1), (-1, 0), (-1, 1), # Координаты контура
+                ( 0, -1), ( 0, 0), ( 0, 1),
+                ( 1, -1), ( 1, 0), ( 1, 1)]
+
+        for d in ship_cont.dots: # Определяем точки коробля
+            for dx, dy in cont: # Провереем координаты вонтура для точки коробля
+                cor = Dot(d.x + dx, d.y + dy) # Смещаем коордитаты точки коробля
+                if not(self.out(cor) and cor not in self.busy): # Точка не выходит за грацъницы и не занята
+                    if tor:
+                        self.fild[cor.x][cor.y] = "."
+                    self.busy.append(cor)
+
+                self.ships.append(ship_cont)
+                self.contour(ship_cont)
+
 
 
 class Ship:  # Родительский класс кораблей
-    def __init__(self, dlina, n_dot, position, live):
+    def __init__(self, n_dot, dlina, position):
         self.dlina = dlina
         self.n_dot = n_dot
         self.position = position
-        self.live = live
+        self.live = dlina
 
-    def get_dots(self):
-        return self.dlina
+    @property
+    def dots(self):
+        ship_dots = []
+        for i in range(self.n_dot):
+            cor_x = self.dlina.x
+            cor_y = self.dlina.y
+            if self.position == 0:
+                cor_x += i
+            if self.position == 1:
+                cor_y += i
+            ship_dots.append(Dot(cor_x, cor_y))
 
-    def get_dots(self):
-        pass
+        return ship_dots
+
 
 
 
@@ -46,3 +110,7 @@ class Kreiser(Ship): # Класс корабля на две клетки пол
 
 class Esminec(Ship): # Клас корабля на одну клетку поля
     pass
+
+
+b = Board()
+print(b)
